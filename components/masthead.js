22ce8ad51e18693c5b5559e4e6f60d3b088b30a3
SetyMedia <setymedia@gmail.com>
2021-02-18 08:33:50 -0500

    Fix masthead height

--- components/masthead.js
+++ components/masthead.js
@@ -1,13 +1,13 @@
 export default function Masthead() {
   return (
-    <div className="relative flex justify-center h-screen overflow-hidden">
+    <div className="relative flex justify-center items-center h-60v overflow-hidden">
       {/* Heading */}
-      <div className="p-36 relative z-30">
+      <div className="relative p-16 z-30 w-auto min-w-full">
         <h1 className="text-7xl font-semibold">
           Hey! My Name is <span className="text-wireframeRed">Imsety</span>.
         </h1>
         <div className="bg-wireframeRed h-2 w-32 my-8"></div>
-        <p className="text-xl font-medium">
+        <p className="text-3xl font-medium">
           I'm a Motion Designer based in Atlanta, GA.
         </p>
       </div>
@@ -16,7 +16,7 @@ export default function Masthead() {
         autoPlay
         loop
         muted
-        className="absolute z-10 w-auto min-w-full min-h-full max-w-none"
+        className="absolute z-10 w-auto min-w-full max-w-none min-h-full"
       >
         <source src="/setymedia-2020-reel-loop.mp4" type="video/mp4" />
         Your browser does not support the video tag.
