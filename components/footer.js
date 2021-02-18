39b2d076e8f97de05d6fe5a69026c5486c9d1bcd
SetyMedia <setymedia@gmail.com>
2021-02-18 09:53:28 -0500

    Install react-icons and import social media link icons

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,28 @@
 import Container from './container';
+import {
+  FaTwitter,
+  FaBehance,
+  FaLinkedinIn,
+  FaInstagram,
+  FaDribbble,
+  FaVimeo,
+  FaYoutube
+} from 'react-icons/fa';
 
 export default function Footer() {
   return (
     <footer className="mt-auto py-8">
       <Container>
         <div className="flex justify-center">
+          <div>
+            <FaTwitter />
+            <FaBehance />
+            <FaLinkedinIn />
+            <FaInstagram />
+            <FaDribbble />
+            <FaVimeo />
+            <FaYoutube />
+          </div>
           <p className="text-gray-300 mt-4">
             © 2021 Imsety Taylor. All rights reserved.
           </p>
