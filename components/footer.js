a67047e7d37de44a7f1bed7769549e431699c8d6
SetyMedia <setymedia@gmail.com>
2021-02-18 09:02:46 -0500

    Move container to each section component

--- components/footer.js
+++ components/footer.js
@@ -1,17 +1,21 @@
+import Container from './Container';
+
 export default function Footer() {
   return (
     <footer className="mt-auto">
-      <a
-        href="https://www.instagram.com/setymedia/"
-        target="_blank"
-        rel="noreferrer"
-        className="text-xl font-bold text-wireframeRed hover:text-frameworkGray underline transition-colors"
-      >
-        Instagram
-      </a>
-      <p className="text-gray-300 mt-4">
-        © 2021 Imsety Taylor. All rights reserved.
-      </p>
+      <Container>
+        <a
+          href="https://www.instagram.com/setymedia/"
+          target="_blank"
+          rel="noreferrer"
+          className="text-xl font-bold text-wireframeRed hover:text-frameworkGray underline transition-colors"
+        >
+          Instagram
+        </a>
+        <p className="text-gray-300 mt-4">
+          © 2021 Imsety Taylor. All rights reserved.
+        </p>
+      </Container>
     </footer>
   );
 }
--- components/masthead.js
+++ components/masthead.js
@@ -1,27 +1,31 @@
+import Container from './Container';
+
 export default function Masthead() {
   return (
-    <div className="relative flex justify-center items-center h-60v overflow-hidden">
-      {/* Heading */}
-      <div className="relative z-30 p-16">
-        <h1 className="text-7xl font-semibold">
-          Hey! My Name is <span className="text-wireframeRed">Imsety</span>.
-        </h1>
-        <div className="bg-wireframeRed h-2 w-32 my-8"></div>
-        <p className="text-3xl font-medium">
-          I'm a Motion Designer based in Atlanta, GA.
-        </p>
+    <Container>
+      <div className="relative flex justify-center items-center h-60v overflow-hidden">
+        {/* Heading */}
+        <div className="relative z-30 p-16">
+          <h1 className="text-7xl font-semibold">
+            Hey! My Name is <span className="text-wireframeRed">Imsety</span>.
+          </h1>
+          <div className="bg-wireframeRed h-2 w-32 my-8"></div>
+          <p className="text-3xl font-medium">
+            I'm a Motion Designer based in Atlanta, GA.
+          </p>
+        </div>
+        <div className="absolute z-20 bg-outlineWhite bg-opacity-90 min-w-full min-h-full"></div>
+        {/* Video */}
+        <video
+          autoPlay
+          loop
+          muted
+          className="absolute z-10 w-auto min-w-full max-w-none min-h-full"
+        >
+          <source src="/setymedia-2020-reel-loop.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
-      <div className="absolute z-20 bg-outlineWhite bg-opacity-90 min-w-full min-h-full"></div>
-      {/* Video */}
-      <video
-        autoPlay
-        loop
-        muted
-        className="absolute z-10 w-auto min-w-full max-w-none min-h-full"
-      >
-        <source src="/setymedia-2020-reel-loop.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
+    </Container>
   );
 }
