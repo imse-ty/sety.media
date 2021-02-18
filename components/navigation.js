4bcc6f6d57c12c36af40224bd3b95460076c98fa
SetyMedia <setymedia@gmail.com>
2021-02-18 09:34:03 -0500

    Move list items to a "MenuItem" component

--- components/navigation.js
+++ components/navigation.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
+function MenuItem(props) {
+  return (
+    <li className="transition pr-9 hover:text-wireframeRed focus:text-wireframeRed">
+      <a href="#">{props.children}</a>
+    </li>
+  );
+}
+
 export default function Navigation() {
   return (
     <nav>
-      <ul>
-        <li>Work</li>
-        <li>Info</li>
-        <li>Blog</li>
+      <ul className="flex text-xl font-semibold text-frameworkGray">
+        <MenuItem>Work</MenuItem>
+        <MenuItem>Info</MenuItem>
+        <MenuItem>Blog</MenuItem>
       </ul>
     </nav>
   );
