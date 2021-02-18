4c67bd30299489621c55571742a0abbaa104f64a
SetyMedia <setymedia@gmail.com>
2021-02-18 09:16:03 -0500

    Add SetyMedia brandmark to header and adjust top and bottom padding

--- components/header.js
+++ components/header.js
@@ -4,7 +4,15 @@ import Container from './container';
 export default function Header() {
   return (
     <header>
-      <Container>Hi</Container>
+      <Container>
+        <div className="py-8">
+          <img
+            src="./setymedia-brandmark-black-on-red.svg"
+            className="w-20"
+            alt="SetyMedia logo"
+          />
+        </div>
+      </Container>
     </header>
   );
 }
